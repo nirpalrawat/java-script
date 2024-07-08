58626d91a0af2501f5163701fbff289d171c0ec7
nirpal rawat <167707880+nirpalrawat@users.noreply.github.com>
2024-07-09 01:56:52 +0530

    Add files via upload

---
+++ javascript class/main.js
@@ -0,0 +1,1 @@
+document.write("helle java script oyu bhiuty b ")
---
+++ javascript class/set.js
@@ -0,0 +1,16 @@
+//document.write("hello")
+
+//alert("hello gwalior")
+
+//document.getElementById("a").innerHTML="<center> <h2> pninfosy gwalior 1234</h2></center>"
+
+//document.getElementById("a").setAttribute("style"," border:10px dotted red");
+
+//document.getElementById("b").removeAttribute("style");
+//document.getElementById("a").innerHTML="<h1> gwalior</h1>";
+
+ //querySelector
+
+
+document.querySelector("#a").innerHTML="<center> <h2> pninfosy gwalior 1234</h2></center>"
+document.querySelector('.c').innerHTML="<h1> hello gwalior 1234655465</h1>"
